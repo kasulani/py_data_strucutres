7ee591db4d5abf265e4d07141490b798253cc3c5
Emmanuel King Kasulani <eka@hellofresh.com>
2019-10-07 17:20:15 +0200

    add __eq__ to compare two nodes for equality

--- linked_list/nodes.py
+++ linked_list/nodes.py
@@ -16,6 +16,9 @@ class BaseNode(object):
     def __str__(self):
         return "{}".format(self.data)
 
+    def __eq__(self, other):
+        return str(self) == str(other)
+
     def __repr__(self):
         return "Node <data: {} - next: {}>".format(self._data, self._next)
 
